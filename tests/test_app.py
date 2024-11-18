af3156670861e328db3c8538c074ed1a75ff0b58
Readiee <galsanov.bul888@gmail.com>
2024-11-18 17:14:03 +0300

    lab6 - CI/CD (imports fixed x2, demonstrative tests added)

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,12 +1,19 @@
 import pytest
-import app
+from app import app
+
 
 @pytest.fixture
 def client():
     app.testing = True
     return app.test_client()
 
-def test_get_books(client):
-    response = client.get('/books')
-    assert response.status_code == 200
-    assert isinstance(response.json, list)
+# def test_get_books(client):
+#     response = client.get('/books')
+#     assert response.status_code == 200
+#     assert isinstance(response.json, list)
+
+def test_success():
+    assert 200 == 200
+
+def test_fail():
+    assert 500 == 200
